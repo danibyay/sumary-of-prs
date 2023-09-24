5e3ba98992fec42cc57e2556abfdaaa5a5020bb0
Daniela Becerra Gonzalez <danybecerr@gmail.com>
2023-09-24 01:26:20 -0600

    add gitignore and gmail sending is working

--- program.py
+++ program.py
@@ -4,6 +4,18 @@ from datetime import datetime, timedelta
 from dateutil import parser, tz 
 import sys
 
+
+import base64
+from email.message import EmailMessage
+
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
 # Use the github API to list all pull requests from a given repository
 # Returns a list of dictionaries, each pull request object is a dictionary
 def get_pull_requests_data(owner, repo, token):
@@ -15,7 +27,6 @@ def get_pull_requests_data(owner, repo, token):
   }
   response = requests.get(api_url, headers=headers)
   return response.json()
-
 
 # Filter the list of PRs to only have those that have been created in the last week
 # Returns a list of dictionaries
@@ -53,30 +64,97 @@ def get_human_date(date_string):
   datetime_object = parser.parse(date_string)
   return f"{datetime_object.month}/{datetime_object.day}"
 
-# Print the summary to the console
-# MVP before sending an email
-def print_summary_to_console(list_of_prs):
-  print("Summary of last week's PRs\n")
+# Build a string of the summary to be printed or sent as email
+def build_summary_message(list_of_prs):
+  message = "Summary of last week's PRs\n"
   for i in range(len(list_of_prs)):
     title = list_of_prs[i]["title"]
     state = list_of_prs[i]["state"]
     date = get_human_date(list_of_prs[i]["created_at"])
     is_draft = list_of_prs[i]["is_draft"]
     url = list_of_prs[i]["html_url"]
-    print(f"\n{i+1}. {title}")
-    print(f"PR status is: {state}")
-    print(f"PR was created on {date}")
-    print(f"PR is draft: {is_draft}")
-    print(f"Read more at: {url}")
+    message += f"\n{i+1}. {title}\n"
+    message += f"PR status is: {state}\n"
+    message += f"PR was created on {date}\n"
+    message += f"PR is draft: {is_draft}\n"
+    message += f"Read more at: {url}\n"
+  return message
 
-def main():
+# Print the summary to the console
+# MVP before sending an email
+def print_summary_to_console(list_of_prs):
+  print(build_summary_message(list_of_prs))
+
+
+# Call all the pull requests related functions to return the summary string
+def get_pr_summary():
   all_prs = get_pull_requests_data("opentofu", "opentofu", str(sys.argv[1]))
   desired_prs = filter_prs_by_age(all_prs)
   pr_extracted_metadata = get_pr_relevant_metadata(desired_prs)
-  print_summary_to_console(pr_extracted_metadata)
+  message = build_summary_message(pr_extracted_metadata)
+  return message
+
+SCOPES = ['https://www.googleapis.com/auth/gmail.send']
+
+def get_oauth_creds():
+  creds = None
+  # The file token.json stores the user's access and refresh tokens, and is
+  # created automatically when the authorization flow completes for the first
+  # time.
+  if os.path.exists('token.json'):
+      creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+  # If there are no (valid) credentials available, let the user log in.
+  if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+      creds.refresh(Request())
+    else:
+      flow = InstalledAppFlow.from_client_secrets_file(
+          'credentials.json', SCOPES)
+      creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open('token.json', 'w') as token:
+      token.write(creds.to_json())
+    
+  return creds
+
+
+  # Create and send an email message
+  # Print the returned  message id
+  # Returns: Message object, including message id
+  # use oauth creds
+  
+def gmail_send_message():
+  try:
+    service = build('gmail', 'v1', credentials=get_oauth_creds())
+    message = EmailMessage()
+
+    message.set_content(get_pr_summary())
+
+    message['To'] = 'danybecerr+testing@gmail.com'
+    message['From'] = 'madajiji@gmail.com'
+    message['Subject'] = 'This week\'s pull requests summary'
+
+    # encoded message
+    encoded_message = base64.urlsafe_b64encode(message.as_bytes()) \
+        .decode()
+
+    create_message = {
+        'raw': encoded_message
+    }
+    # pylint: disable=E1101
+    send_message = (service.users().messages().send
+                    (userId="me", body=create_message).execute())
+    print(F'Message Id: {send_message["id"]}')
+  except HttpError as error:
+    print(F'An error occurred: {error}')
+    send_message = None
+  return send_message
+
+
+
 
 if __name__ == "__main__":
-  main()
+  gmail_send_message()
 
 
 
